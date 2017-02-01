a985025656ad27fe3124a819484f7e05e67c71cc
GordonBGood <GordonBGood@users.noreply.github.com>
2017-02-02 00:36:12 +0700

    Native support for hiding mutation...
    Using the JavaScript Closure pattern to hide internal mutation, while as the same time detecting recursive evaluation (failing with an exception if detected) and allowing for the use of the lazyFromValue function to avoid unnecessary deferred evaluation for simple values.
    
    Using the "memoize" function from within a Immediately Invoked Function Expression (IIFE) helped with execution speed, in particular on Chrome V8 after many recursions where it seems that some cache/storage overflows and what optimization there is fails.

--- src/Native/Lazy.js
+++ src/Native/Lazy.js
@@ -1,18 +1,39 @@
 var _elm_lang$lazy$Native_Lazy = function() {
 
-// mutates `lzy` Unevaluated thunk into Evaluated value, returning value.
-function memoize(lzy) {
-    if (lzy.ctor === 'Evaluating')
-        throw Error("Lazy.memoize:  recursive evaluation error!!!");
-    lzy.ctor = 'Evaluating';
-    var v = lzy._0(lzy); // dummy placeholder arg
-    lzy.ctor = 'Evaluated';
-    lzy._0 = v;
-    return v;
+// mutates inside a JS Closure so Elm can't see it.  IIFE helps Chrome speed.
+var memoize = function() {
+    return function(thunk, value) {
+        var _tu;
+        if (value === undefined)
+            _tu = { ctor: 'Unevaluated', _0: thunk }
+        else
+            _tu = { ctor: 'Evaluated', _0: value };
+        return function () {
+            var tag = _tu.ctor;
+            var v = _tu._0;
+            if (tag === 'Evaluating')
+                throw Error("Lazy.force:  recursive evaluation detected!!!");
+            if (tag == 'Unevaluated') {
+                _tu.ctor = 'Evaluating';
+                v = v();
+                _tu = { ctor: 'Evaluated', _0: v };
+            }
+            return v;
+        }
+    };
+}();
+
+function lazy(thunk) {
+    return { ctor: 'Lazy', _0: memoize(thunk, undefined) };
+}
+
+function lazyFromValue(v) {
+    return { ctor: 'Lazy', _0: memoize(undefined, v) };
 }
 
 return {
-    memoize: memoize
+    lazy: lazy,
+    lazyFromValue: lazyFromValue
 };
 
 }();
